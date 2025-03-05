fad038d8743e13b59c87ae5a13495860e41de120
AthulyaMS <athulyamarysaji1996@gmail.com>
2025-03-05 09:11:46 +0530

    removed the upload directory

--- BACKEND/app/crud.py
+++ BACKEND/app/crud.py
@@ -11,10 +11,10 @@ import csv
 
 
 
-UPLOAD_DIR = "uploads/"
-if not os.path.exists(UPLOAD_DIR):
-    os.makedirs(UPLOAD_DIR)
-OUTPUT_DIR = os.path.join(UPLOAD_DIR, "output")  
+# UPLOAD_DIR = "uploads/"
+# if not os.path.exists(UPLOAD_DIR):
+#     os.makedirs(UPLOAD_DIR)
+# OUTPUT_DIR = os.path.join(UPLOAD_DIR, "output")  
 
 
 def parse_usfm_to_csv(book_name, usfm_content, project_id):
